e549304a0edfce8f71f580ee54ba17abc1b60837
Maxim Kiryanov <mxvkiryanov@gmail.com>
2016-06-04 22:56:55 +0300

    Исправил анимацию бэкграунда страницы

--- js/animation.js
+++ js/animation.js
@@ -1,17 +1,19 @@
-var transforms = document.getElementsByClassName("transform");
-console.log(transforms);
-var index = 1;
-setInterval(function () {
-	var imgs = [];
-	for (var i = 0; i < transforms.length; i++) {
-		imgs = transforms[i].getElementsByClassName("transformice");
-		for (var j = 0; j < imgs.length; j++)
-			if (imgs[j].getAttribute("data-order") == index)
-				imgs[j].style.zIndex = 10;
-			else
-				imgs[j].style.zIndex = 5;
-	}
-	index++;
-	if (index > imgs.length)
-		index = 1;
-}, 3000);
+var images = ["bg1.jpg", "bg2.jpg", "bg3.jpg", "bg4.jpg", "bg5.jpg"];
+
+$("div.section").each(function(index, item) {
+    item.style.backgroundImage = getCurrentImageUrl(index);
+});
+
+var currentImage = 0;
+var delay = 3000;
+
+function getCurrentImageUrl(index) {
+    return "url(\"../img/" + images[index] + "\")";
+}
+
+var timeout = setTimeout(function animate() {
+    var activeSection = $("div.section.active");
+    activeSection.css("background-image", getCurrentImageUrl(currentImage++));
+    currentImage = currentImage > 4 ? 0 : currentImage;
+    timeout = setTimeout(animate, delay);
+}, delay);
